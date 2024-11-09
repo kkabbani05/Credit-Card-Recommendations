352bdc836f3d3b30051497bab260dbe7c723e0a7
Kevin Zhang <kevzhang2022@gmail.com>
2024-11-09 17:40:06 -0600

    Finish UI MVP

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import CreditCardOptimizer from "./CreditCardOptimizer";
+
+function App() {
+    return (
+        <div className="App">
+            <CreditCardOptimizer />
+        </div>
+    );
+}
+
+export default App;
